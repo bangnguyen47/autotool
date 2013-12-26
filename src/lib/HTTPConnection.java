372b25fb2e6fd246fe765a019ce905d844315aff
bangnguyen <bangnguyen@cnc.com.vn>
2013-12-26 11:05:14 +0700

    update

--- src/lib/HTTPConnection.java
+++ src/lib/HTTPConnection.java
@@ -1,9 +1,66 @@
 package lib;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class HTTPConnection {
 
+	private List<String> cookies;
+	private HttpsURLConnection conn;
+
+	private final String USER_AGENT = "Mozilla/5.0";
+	
 	public HTTPConnection() {
 		
+	}
+	
+	public String getPageContent(String url) throws Exception {
+
+		URL obj = new URL(url);
+		conn = (HttpsURLConnection) obj.openConnection();
+
+		// default is GET
+		conn.setRequestMethod("GET");
+
+		conn.setUseCaches(false);
+
+		// act like a browser
+		conn.setRequestProperty("User-Agent", USER_AGENT);
+		conn.setRequestProperty("Accept",
+				"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		if (cookies != null) {
+			for (String cookie : this.cookies) {
+				conn.addRequestProperty("Cookie", cookie.split(";", 1)[0]);
+			}
+		}
+		int responseCode = conn.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				conn.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		// Get the response cookies
+		setCookies(conn.getHeaderFields().get("Set-Cookie"));
+
+		return response.toString();
+
+	}
+	
+	public void setCookies(List<String> cookies) {
+		this.cookies = cookies;
 	}
 
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,9 +1,19 @@
 package test;
 
+import lib.HTTPConnection;
+
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println("test");
+	public static void main(String[] args) throws Exception {
+		String url = "http://google.com";
+
+		System.getProperties().put("proxySet", "true");
+		System.getProperties().put("proxyHost", "192.168.0.1");
+		System.getProperties().put("proxyPort", "8080");
+		
+		HTTPConnection http = new HTTPConnection();
+		String s = http.getPageContent(url);
+		System.out.println(s);
 	}
 
 }
